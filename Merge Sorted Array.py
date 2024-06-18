4f450db5434990e87e5bc22a1a87a053b60541a5
DorinB <85808350+DorinBachar@users.noreply.github.com>
2024-06-18 15:07:34 +0300

    Update Merge Sorted Array.py

--- Merge Sorted Array.py
+++ Merge Sorted Array.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        index1, index2 , index = m - 1, n - 1 , m + n -1
-
-        while index2 >= 0:
-            if index1 >=0  and  nums1[index1] > nums2[index2] :
-                nums1[index] = nums1[index1]
-                index1 -= 1
-            else:
-                nums1[index] = nums2[index2]
-                index2 -= 1
-            index -= 1
-
+def merge(self, nums1, m, nums2, n):
+    index1, index2 , index = m - 1, n - 1 , m + n -1
+    while index2 >= 0:
+        if index1 >=0  and  nums1[index1] > nums2[index2] :
+            nums1[index] = nums1[index1]
+            index1 -= 1
+        else:
+            nums1[index] = nums2[index2]
+            index2 -= 1
+        index -= 1
 
 
 def main() :
@@ -20,6 +17,10 @@ def main() :
     n = 3
     merge(nums1, m, nums2, n)
     print(nums1) 
+
+
+if __name__ == "__main__":
+    main()
 
 
   
